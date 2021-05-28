0835d9820d72d3ffad29c3c58bc64d85979ce7d8
khukalenko <evhenii.khukalenko@corp.kismia.com>
2021-05-28 17:22:12 +0300

    locking fixed

--- optimistic_locking.py
+++ optimistic_locking.py
@@ -8,15 +8,16 @@ client = hazelcast.HazelcastClient(
         "127.0.0.1:5703",
     ]
 )
-my_map = client.get_map("lab3_4")
+my_map = client.get_map("lab3_8dfddedrf")
 key = '1'
-my_map.put(key, 0)
+my_map.put_if_absent(key, 0)
 print('starting')
-for i in range(1000):
+for i in range(10):
     while True:
         old_value = my_map.get(key).result()
         new_value = int(old_value) + 1
-        if my_map.replace_if_same(key, old_value, new_value):
+        print(new_value)
+        if my_map.replace_if_same(key, old_value, new_value).result():
             break
 
 print(" Optimistic locking Finished! Result = " + str(my_map.get(key).result()))
--- pessimistic_locking.py
+++ pessimistic_locking.py
@@ -8,19 +8,16 @@ client = hazelcast.HazelcastClient(
         "127.0.0.1:5703",
     ]
 )
-my_map = client.get_map("lab3_4")
-key='1'
-my_map.put(key, 0)
+my_map = client.get_map("lab3_8dfddedrf")
+key = '1'
+my_map.put_if_absent(key, 0)
 print('starting')
-for i in range(1000):
-    my_map.lock(key)
-    try:
-        value = my_map.get(key).result()
-        value = int(value) + 1
-        my_map.put(key, value)
-    finally:
-        my_map.unlock(key)
+for i in range(10):
+    while True:
+        old_value = my_map.get(key).result()
+        new_value = int(old_value) + 1
+        print(new_value)
+        if my_map.replace_if_same(key, old_value, new_value).result():
+            break
 
-print("Pessimistic locking Finished! Result = " + str(my_map.get(key).result()))
-
-
+print(" Optimistic locking Finished! Result = " + str(my_map.get(key).result()))
